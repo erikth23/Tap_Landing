b42fc35874dbfbcef2a8d9567367e9f7875210c0
Erik Thomas-Hommer <erikth2355@gmail.com>
2021-08-16 10:59:21 -0700

    Updating name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,18 @@
 require(`dotenv`).config()
 
 module.exports = {
-  siteMetadata: {
-    title: "Tap",
-    description: "Tap Systems Landing Page",
-    siteUrl: process.env.SITE_URL || "http://localhost:8000",
-  },
-  plugins: [
-    "@reflexjs/gatsby-theme-base",
-    "@reflexjs/gatsby-theme-post",
-    {
-      resolve: "@reflexjs/gatsby-plugin-metatags",
-      options: {
-        types: [`Page`, `Post`],
-      },
-    },
-  ],
+	siteMetadata: {
+		title: "Tap Tech Systems",
+		description: "Tap Tech Systems Landing Page",
+		siteUrl: process.env.SITE_URL || "http://localhost:8000"
+	},
+	plugins: [
+		"@reflexjs/gatsby-theme-base",
+		"@reflexjs/gatsby-theme-post", {
+			resolve: "@reflexjs/gatsby-plugin-metatags",
+			options: {
+				types: [`Page`, `Post`]
+			}
+		}
+	]
 }
